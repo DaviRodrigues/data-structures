fd6c3457eb12256258b40b208db0330431243364
DaviRodrigues <oversouls11@gmail.com>
2026-09-10 15:57:39 -0300

    feat: update theoretical exercises and implement recursive binary search

--- modulo_02_divisao_e_hash/cap_04_quicksort/exercicios.py
+++ modulo_02_divisao_e_hash/cap_04_quicksort/exercicios.py
@@ -7,7 +7,9 @@ Livro: Entendendo Algoritmos
 # Por que o Quicksort tem desempenho O(n^2) no pior caso se escolhermos sempre o primeiro elemento
 # como pivô em um array que JÁ ESTÁ ORDENADO? (Ex: [1, 2, 3, 4, 5])
 RESPOSTA_EXERCICIO_1 = """
-Sua resposta aqui:
+Sua resposta aqui: O problema aqui que não tem uma verificação se o array previamente está ordenado.
+Nesse caso vamos acabar passando O(n^2) uma para ir até o fim do array e outra para voltar e fazer a ordem.
+Isso acaba sendo péssimo, principalmente se o pivot for o primeiro item, que causa ir de um em um item
 """
 
 
@@ -15,7 +17,10 @@ Sua resposta aqui:
 # A notação Big O para o Merge Sort é SEMPRE O(n log n), enquanto o Quicksort pode ser O(n^2) no pior caso.
 # Mesmo assim, por que no mundo real muitos desenvolvedores e linguagens preferem o Quicksort ao invés do Merge Sort?
 RESPOSTA_EXERCICIO_2 = """
-Sua resposta aqui:
+Sua resposta aqui: A preferência geralmente é devido ao quicksort ter seu modelo de implentação in-place,
+onde mantemos o espaço na memória constante e o que é influênciado nesse caso seria apenas as chamadas de funções na recursão.
+Além disso, temos um uso melhor de cache, visto a referência de memória única, por mais que o quicksort possa no pior caso se O(n^2)
+muita das vezes isso não vai acontecer se escolhido um bom pivot.
 """
 
 
@@ -30,12 +35,24 @@ Sua resposta aqui:
 # - Caso recursivo: Se lista[meio] > item, chame recursivamente na metade inferior (alto = meio - 1).
 #                   Se lista[meio] < item, chame recursivamente na metade superior (baixo = meio + 1).
 def pesquisa_binaria_recursiva(
-    lista: list, item: int, baixo: int = 0, alto: int = None
+    lista: list, item: int, baixo: int = 0, alto: int | None = None
 ) -> int | None:
     if alto is None:
         alto = len(lista) - 1
 
-    # SEU CÓDIGO AQUI
+    if baixo > alto:
+        return None
+    
+    meio = (alto + baixo) // 2
+    aux = lista[meio]
+        
+    if aux == item:
+        return meio
+        
+    if aux > item:
+        return pesquisa_binaria_recursiva(lista, item, baixo, meio - 1)
+    else:
+        return pesquisa_binaria_recursiva(lista, item, meio + 1, alto)
 
 
 if __name__ == "__main__":
